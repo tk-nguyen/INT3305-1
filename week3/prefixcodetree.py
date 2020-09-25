3a1af3b9161dd2f8fb247501805031c5318eaeb1
Thai Nguyen <shiroemon279@gmail.com>
2020-09-25 18:00:34 +0700

    Updated the tree

--- week3/prefixcodetree.py
+++ week3/prefixcodetree.py
@@ -1,29 +1,46 @@
 class Node():
-    def __init__(self, value):
-        self.value = value
-        self.left = None 
+    def __init__(self, name):
+        self.name = name
+        self.left = None
         self.right = None
+
 
 class PrefixCodeTree():
     def __init__(self):
-        self.root = None
+        self.root = Node("")
 
     def insert(self, codeword, symbol):
-        if self.root is None:
-            if codeword[0] == 0:
-                self.root = Node(None)
-                self.root.left = Node(symbol)
-            elif codeword[0] == 1:
-                self.root = Node(None)
-                self.root.right = Node(symbol)
-        else:
-            if len(codeword) == 1:
-                if codeword[0] == 0:
-                    self.left = Node(symbol)
-                elif codeword[0] == 1:
-                    self.right = Node(symbol)
+        tmp = self.root
+        # Inserting nodes into the trees. 0 is a left node, 1 is right
+        for i in range(0, len(codeword)):
+            if i == len(codeword) - 1:
+                if codeword[i] == 0:
+                    tmp.left = Node(symbol)
+                elif codeword[i] == 1:
+                    tmp.right = Node(symbol)
             else:
-                self.insert(codeword[1:], symbol)
-        
+                if codeword[i] == 0:
+                    if tmp.left is None:
+                        tmp.left = Node("")
+                    tmp = tmp.left
+                elif codeword[i] == 1:
+                    if tmp.right is None:
+                        tmp.right = Node("")
+                    tmp = tmp.right
+
     def decode(self, encodedData, datalen):
-        return None
+        # Convert byte to bitstring
+        bitstring = "".join(f"{byte:0>8b}" for byte in encodedData)[:datalen]
+        message = ""
+        tmp = self.root
+        # Traverse down the tree according to bits
+        for bit in bitstring:
+            if int(bit) == 0:
+                tmp = tmp.left
+            elif int(bit) == 1:
+                tmp = tmp.right
+
+            if tmp.name != "":
+                message += tmp.name
+                tmp = self.root
+        return message
--- week3/test.py
+++ week3/test.py
@@ -10,13 +10,9 @@ codeTree = PrefixCodeTree()
 
 for symbol in codebook:
     codeTree.insert(codebook[symbol], symbol)
-print(codeTree.Node.left.name)
-#message = codeTree.decode(b'\xd2\x9f\x20', 21)
-# message = x4x1x2x3x1x1x4x4x2x2
-# Below is explaination:
-#  data = b'\xd2\x9f\x20' --> 24 bit
-#  datalen = 21 --> use only 21 bit, 3 remaining bits are not used
-#
-#  Decoded data should be
-#  11 0  100 10|1 0  0  11 11 1|00 100 000
-#  x4 x1 x2  x3   x1 x1 x4 x4 x2   x2
+
+original = b'\xd2\x9f\x20'
+decoded = "x4x1x2x3x1x1x4x4x2x2"
+message = codeTree.decode(original, 21)
+print(message)
+print(message == decoded)
